f667c7af8d135374aeb08b102b649e8f1a39ce75
xulei1_cop <xulei-ici@inspurworld.com>
2023-08-27 16:41:10 +0800

    changes

--- main/main.go
+++ main/main.go
@@ -1,14 +1,9 @@
 package main
 
 import (
-	// "fmt"
 	"Management/core"
-	// "Management/utils"
 )
 
 func main() {
 	core.Run()
-	// core.LoadInfo()
-	// content, err := utils.FileRead()
-	// fmt.Println("content: ", content, "error: ", err)
 }
